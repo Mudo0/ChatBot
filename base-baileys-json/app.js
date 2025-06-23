6af8667a2f729d6aef91570397eb5e9f90550120
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-06-23 14:40:29 -0300

    whatsapp bot basico
    -whatsapp bot basico con datos guardados en .json
    
    -1 sola respuesta en flowPrincipal()
    
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ base-baileys-json/app.js
@@ -0,0 +1,25 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const JsonFileAdapter = require('@bot-whatsapp/database/json')
+
+const iniciadores = ["hola", "buenos días", "buenas tardes", "buenas noches", "qué tal", "cómo estás"]
+
+const flowPrincipal = addKeyword(iniciadores).addAnswer('Hola, ¿en qué puedo ayudarte?')
+
+const main = async () => {
+    const adapterDB = new JsonFileAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
